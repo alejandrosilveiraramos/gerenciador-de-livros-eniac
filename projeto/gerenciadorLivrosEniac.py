cf41e0b10f8a0b965bb0271d56f0db585e590e23
Alejandro Silveira Ramos <alejandrosilveiraramos3@gmail.com>
2022-09-21 20:20:45 -0300

    add feat: isbn validacao tentando

--- projeto/gerenciadorLivrosEniac.py
+++ projeto/gerenciadorLivrosEniac.py
@@ -1,11 +1,43 @@
-#Dev (Teste de codigos antes da homolog) Testar conjunto de codigos
+# Dev
 
-#menu // Felipe
+'''
+Opção criar livro:
 
-#1 cadastro livro // Alejandro
+    - O programa deve perguntar as informações cadastrais, fazer a verificação do isbnLivro e, caso esteja tudo nos conformes,
 
-#2 listar os livros cadastrados // David
+    adicionar o livro mostrando uma mensagem de sucesso;
 
-#3 excluir o livro cadastrado // Marcio e Adriano
+    - Caso tenha algum problema com o ISBN, o código deverá mostrar uma mensagem de erro e não cadastrar o livro, mostrando
 
-#4 finalizar o programa // MC vargas
+    o menu novamente;
+
+    - Os dados cadastrais do livro constam nos requisitos abaixo;
+'''
+
+import re
+
+
+while True:
+    print('Vamos cadastrar seu novo livro')
+
+    '''nomeLivro = str(input('Nome do livro: '))
+    editoraLivro = str(input('Nome da editora: '))
+    autorLivro = str(input('Nome do autor: '))
+    generoLivro = str(input('Genero do livro: '))
+    anoLivro = str(input('Ano do livro: '))'''
+    isbnLivro = str(input('ISBN do livro: '))
+
+    while True:
+       
+        isbnLivro = isbnLivro.replace("-", "").replace(" ", "").upper();
+        match = re.search(r'^(\d{9})(\d|X)$', isbnLivro)
+        if not match:
+            print('deuruim')
+    
+        digits = match.group(1)
+        check_digit = 10 if match.group(2) == 'X' else int(match.group(2))
+
+        result = sum((i + 1) * int(digit) for i, digit in enumerate(digits))
+        if (result % 11) == check_digit:
+            print('deuboa')
+            break
