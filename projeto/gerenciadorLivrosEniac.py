92d592ea0ef1f3a90b73524f2dcdada0d4a6b0d9
FelipeWeiss1992 <felipeweiss92@gmail.com>
2022-09-21 20:19:11 -0300

    add feat: Menu Update2

--- projeto/gerenciadorLivrosEniac.py
+++ projeto/gerenciadorLivrosEniac.py
@@ -13,18 +13,48 @@ from time import sleep
 
 decoracao = '-=' * 5 
 
-while True:
-        try:
-            print(decoracao, 'Cadastro de Livros', decoracao)
-            opcaoMenu = str(input('''\nEscolha uma das opções abaixo:
+livraria = []
+livro = {}
+
+opcaoMenu = 0
+
+while opcaoMenu != 4:
+    try:
+        print(decoracao, 'Cadastro de Livros', decoracao)
+        opcaoMenu = int(input('''\nEscolha uma das opções abaixo:
 1 -> Cadastrar Livro
 2 -> Listar Livros
 3 -> Excluir Livros
 4 -> Encerrar o programa
-Qual opção: '''))
-        except ValueError:
-            print('Opcão inválida\n')
-            sleep(1)
+Qual opção: '''))  
+        while True:
+            if opcaoMenu == 1:  
+                livro['Nome'] =  str(input('Nome do Livro: '))
+                livro['Editora'] =  str(input('Editora: '))
+                livro['Autor'] =  str(input('Autor: '))
+                livro['Genero'] =  str(input('Genêro: '))
+                livro['Ano Publicação'] =  int(input('Ano de Publicação: '))
+                livro['ISBN'] =  int(input('ISBN: '))
+
+                livraria.append(livro.copy())
+                while True:
+                    resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+                    if resp in 'SN':
+                        break
+                    print('ERRO! Responda apensa S ou N')
+                if resp in 'N':
+                    break 
+            else:
+                print('Opção inválida')
+                sleep(1)
+                break          
+    except:
+        print('Opcão inválida\n')
+        sleep(1) 
+
+
+
+
         
                  
               
