7fcedd608e68c161f0ee1f8eaac85c46ef40749c
FelipeWeiss1992 <felipeweiss92@gmail.com>
2022-09-21 19:12:08 -0300

    add feat: Menu Update

--- projeto/gerenciadorLivrosEniac.py
+++ projeto/gerenciadorLivrosEniac.py
@@ -9,3 +9,23 @@
 #3 excluir o livro cadastrado // Marcio e Adriano
 
 #4 finalizar o programa // MC vargas
+from time import sleep
+
+decoracao = '-=' * 5 
+
+while True:
+        try:
+            print(decoracao, 'Cadastro de Livros', decoracao)
+            opcaoMenu = str(input('''\nEscolha uma das opções abaixo:
+1 -> Cadastrar Livro
+2 -> Listar Livros
+3 -> Excluir Livros
+4 -> Encerrar o programa
+Qual opção: '''))
+        except ValueError:
+            print('Opcão inválida\n')
+            sleep(1)
+        
+                 
+              
+    
