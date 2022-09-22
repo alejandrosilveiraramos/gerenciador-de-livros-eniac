56ce204452970315620a6e4a37428720ec7748bb
Alejandro Silveira Ramos <alejandrosilveiraramos3@gmail.com>
2022-09-21 22:02:07 -0300

    add feat: ISBN validador

--- projeto/gerenciadorLivrosEniac.py
+++ projeto/gerenciadorLivrosEniac.py
@@ -1,9 +1,11 @@
+import re
+
 # Dev
 
 '''
 Opção criar livro:
 
-    - O programa deve perguntar as informações cadastrais, fazer a verificação do isbnLivro e, caso esteja tudo nos conformes,
+    - O programa deve perguntar as informações cadastrais, fazer a verificação do isbnLivroLivro e, caso esteja tudo nos conformes,
 
     adicionar o livro mostrando uma mensagem de sucesso;
 
@@ -13,9 +15,6 @@ Opção criar livro:
 
     - Os dados cadastrais do livro constam nos requisitos abaixo;
 '''
-
-import re
-
 
 while True:
     print('Vamos cadastrar seu novo livro')
@@ -27,17 +26,45 @@ while True:
     anoLivro = str(input('Ano do livro: '))'''
     isbnLivro = str(input('ISBN do livro: '))
 
-    while True:
-       
-        isbnLivro = isbnLivro.replace("-", "").replace(" ", "").upper();
-        match = re.search(r'^(\d{9})(\d|X)$', isbnLivro)
-        if not match:
-            print('deuruim')
-    
-        digits = match.group(1)
-        check_digit = 10 if match.group(2) == 'X' else int(match.group(2))
+    isbnLivro = isbnLivro.replace("-","") 	# remove -
+    isbnLivro = isbnLivro.replace(" ","") 	# remove whitespace
 
-        result = sum((i + 1) * int(digit) for i, digit in enumerate(digits))
-        if (result % 11) == check_digit:
-            print('deuboa')
-            break
+    #complile regex expressions
+    type10 = re.compile(r"^\d{10}$") # 10 digit
+    type10x = re.compile(r"^\d{9}X$") # 9 digit with X
+    type13 = re.compile(r"^\d{13}$") # 13 digit
+
+    success = "ISBNLisbnLivro is valid!"
+    fail = "ISBNLisbnLivro is invalid."
+
+    #10 digit ISBNLisbnLivro logic
+    if type10.match(isbnLivro) or type10x.match(isbnLivro):
+        factor = len(isbnLivro)
+        total = 0
+        for digit in isbnLivro:
+            if digit == "X": 
+                digit = 10
+            total += (int(digit)*factor)
+            factor -= 1
+        if total % 11 == 0 :
+            print('success')
+        else:
+            print("fail")
+
+    #13 digit ISBNLisbnLivro logic
+    elif type13.match(isbnLivro):
+        total = 0
+        counter = 1
+        for digit in isbnLivro:
+            digit = int(digit)
+            if counter % 2 == 0:
+                digit = digit * 3
+            counter += 1
+            total += digit
+        if total % 10 == 0 :
+            print ('success')
+        else:
+            print("fail")
+    else:  #neither 10 nor 13 digits
+        print("fail")
+
