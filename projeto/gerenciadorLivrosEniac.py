9555b05a7a87db9ca37e08324fad378da35d5bda
Alejandro Silveira Ramos <alejandrosilveiraramos3@gmail.com>
2022-09-23 19:57:39 -0300

    all dev

--- projeto/gerenciadorLivrosEniac.py
+++ projeto/gerenciadorLivrosEniac.py
@@ -1,4 +1,5 @@
 import re
+from time import sleep
 
 
 print( '''                                                                
@@ -28,78 +29,6 @@ print( '''
 ░░                                                                           
       ''')
 
-
-while True:
-    print('Vamos cadastrar seu novo livro')
-
-    #ficha cadastral
-    nomeLivro = str(input('Nome do livro: '))
-    editoraLivro = str(input('Nome da editora: '))
-    autorLivro = str(input('Nome do autor: '))
-    generoLivro = str(input('Genero do livro: '))
-    anoLivro = str(input('Ano do livro: '))
-    
-    #validacao ISBN
-    while True:
-        
-        isbnLivro = str(input('ISBN do livro: '))
-
-        isbnLivro = isbnLivro.replace('-','') 	# remove -
-        isbnLivro = isbnLivro.replace(' ','') 	# remove espaco
-        
-        #compilando regex
-        digito10 = re.compile(r'^\d{10}$') # 10 dígitos
-        digito10x = re.compile(r'^\d{9}X$') # 9 dígitos com X
-        digito13 = re.compile(r'^\d{13}$') # 13 dígitos
-
- producao-alejandro
-        sucesso = 'ISBN valido!'
-        falhou = 'ISBN não valido. \nTente novamente...'
-
-        #10 dígitos ISBN
-        if digito10.match(isbnLivro) or digito10x.match(isbnLivro):
-            fator = len(isbnLivro)
-            total = 0
-            
-            for digit in isbnLivro:
-                if digit == 'X': 
-                    digit = 10
-                total += (int(digit)*fator)
-                fator -= 1
-                
-            if total % 11 == 0 :
-                print(sucesso)
-                break
-            else:
-                print(falhou)
-
-
-        #13 dígito ISBN
-        elif digito13.match(isbnLivro):
-            total = 0
-            contador = 1
-            for digit in isbnLivro:
-                digit = int(digit)
-                if contador % 2 == 0:
-                    digit = digit * 3
-                contador += 1
-                total += digit
-            if total % 10 == 0 :
-                print(sucesso)
-                break
-            else:
-                print(falhou)
-                
-        else:  #Nem 10 nem 13 digits
-            print(falhou)
-            
-    break
-
-print('deu boa')
-
-#4 finalizar o programa // MC vargas
-from time import sleep
-
 decoracao = '-=' * 5 
 
 livraria = []
@@ -116,7 +45,79 @@ while opcaoMenu != 4:
 4 -> Encerrar o programa
 Qual opção: '''))  
         if opcaoMenu == 1:
-            print('1')
+            
+            while True:
+                print('Vamos cadastrar seu novo livro')
+
+                #ficha cadastral
+                nomeLivro = str(input('Nome do livro: '))
+                editoraLivro = str(input('Nome da editora: '))
+                autorLivro = str(input('Nome do autor: '))
+                generoLivro = str(input('Genero do livro: '))
+                anoLivro = str(input('Ano do livro: '))
+                
+                #validacao ISBN
+                while True:
+                    
+                    isbnLivro = str(input('ISBN do livro: '))
+
+                    isbnLivro = isbnLivro.replace('-','') 	# remove -
+                    isbnLivro = isbnLivro.replace(' ','') 	# remove espaco
+                    
+                    #compilando regex
+                    digito10 = re.compile(r'^\d{10}$') # 10 dígitos
+                    digito10x = re.compile(r'^\d{9}X$') # 9 dígitos com X
+                    digito13 = re.compile(r'^\d{13}$') # 13 dígitos
+
+                
+                    sucesso = 'ISBN valido!'
+                    falhou = 'ISBN não valido. \nTente novamente...'
+
+                    #10 dígitos ISBN
+                    if digito10.match(isbnLivro) or digito10x.match(isbnLivro):
+                        fator = len(isbnLivro)
+                        total = 0
+                        
+                        for digit in isbnLivro:
+                            if digit == 'X': 
+                                digit = 10
+                            total += (int(digit)*fator)
+                            fator -= 1
+                            
+                        if total % 11 == 0 :
+                            print(sucesso)
+                            break
+                        else:
+                            print(falhou)
+
+
+                    #13 dígito ISBN
+                    elif digito13.match(isbnLivro):
+                        total = 0
+                        contador = 1
+                        for digit in isbnLivro:
+                            digit = int(digit)
+                            if contador % 2 == 0:
+                                digit = digit * 3
+                            contador += 1
+                            total += digit
+                        if total % 10 == 0 :
+                            print(sucesso)
+                            break
+                        else:
+                            print(falhou)
+                            
+                    else:  #Nem 10 nem 13 digits
+                        print(falhou)
+                    
+                
+                resp = ' '
+                while resp not in 'SsNn':
+                    resp = str(input('Deseja cadastrar outro livro? '))
+                    
+                if resp in 'Nn':
+                    break
+            
         elif opcaoMenu == 2:
             print('2')
         elif opcaoMenu == 3:
@@ -126,6 +127,11 @@ Qual opção: '''))
         else:
             print('Opcão inválida\n')
             sleep(1) 
+
+
+
+
+
 
 
 
