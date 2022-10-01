9999c5e2ed6efd9d5a7a772c69fa728681868fc3
Alejandro Silveira Ramos <alejandrosilveiraramos3@gmail.com>
2022-10-01 14:53:48 -0300

    initial

--- project/funcitions/menu.py
+++ project/funcitions/menu.py
@@ -1,6 +1,23 @@
-def ola():
-    print('oi')
+def menu():
+    
+    print(decoracao, '\033[93mGERENCIADOR DE LIVROS GUIDOLOOPING\033[m', decoracao)
+    while opcaoMenu != '4':
+            
+            if opcaoMenu == '1':
+                print('1')
 
+                
+            elif opcaoMenu == '2':
+                print('2')
+    
+            elif opcaoMenu == '3':
+                print('3')
 
+                
+            elif opcaoMenu == '4':
+                print('\n\033[32mSistema finalizado com sucesso.\033[m\n')
+                
+            else:
+                print('\n\033[31mOpcão inválida, tente novamente!\033[m\n')
 
 
